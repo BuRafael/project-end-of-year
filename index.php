613814f6059ddea2f7c40416e78eb8819aca7dbb
PounPoun333 <kaoripanzani@gmail.com>
2025-12-11 12:03:30 +0100

    fiche série

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file
+ */
+
+get_header();
+?>
+
+<main id="main" class="site-main">
+    <?php
+    if (have_posts()) :
+        while (have_posts()) :
+            the_post();
+            the_content();
+        endwhile;
+    else :
+        echo '<p>Aucun contenu trouvé.</p>';
+    endif;
+    ?>
+</main>
+
+<?php
+get_footer();
+?>
